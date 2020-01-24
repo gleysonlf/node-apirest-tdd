049c9a3baf07e07d079153a9bd1652c6be1354b7
Gleyson Lira <gleysonlf@live.com>
2020-01-24 20:24:58 -0300

    Fixed description of test example

--- test/jest.test.js
+++ test/jest.test.js
@@ -1,20 +1,20 @@
 // fetch = jest.fn(() => Promise.resolve());
 
-test('Principais funções do Jest', () => {
+test('Jest Example - Principais funções do Jest', () => {
     expect(1 + 2).toBe(3);
 });
 
-test('two plus two is four', () => {
+test('Jest Example - two plus two is four', () => {
     expect(2 + 2).toBe(4);
 });
 
-test('object assignment', () => {
+test('Jest Example - object assignment', () => {
     const data = { one: 1 };
     data['two'] = 2;
     expect(data).toEqual({ one: 1, two: 2 });
 });
 
-test('adding positive numbers is not zero', () => {
+test('Jest Example - adding positive numbers is not zero', () => {
     for (let a = 1; a < 10; a++) {
         for (let b = 1; b < 10; b++) {
             expect(a + b).not.toBe(0);
@@ -30,7 +30,7 @@ test('adding positive numbers is not zero', () => {
 // toBeTruthy - matches anything that an if statement treats as true
 // toBeFalsy - matches anything that an if statement treats as false
 
-test('null', () => {
+test('Jest Example - null', () => {
     const n = null;
     expect(n).toBeNull();
     expect(n).toBeDefined();
@@ -39,7 +39,7 @@ test('null', () => {
     expect(n).toBeFalsy();
 });
 
-test('zero', () => {
+test('Jest Example - zero', () => {
     const z = 0;
     expect(z).not.toBeNull();
     expect(z).toBeDefined();
@@ -50,7 +50,7 @@ test('zero', () => {
 
 // REFERENCES TO NUMBERS
 
-test('two plus two', () => {
+test('Jest Example - two plus two', () => {
     const value = 2 + 2;
     expect(value).toBeGreaterThan(3);
     expect(value).toBeGreaterThanOrEqual(3.5);
@@ -62,7 +62,7 @@ test('two plus two', () => {
     expect(value).toEqual(4);
 });
 
-test('adding floating point numbers', () => {
+test('Jest Example - adding floating point numbers', () => {
     const value = 0.1 + 0.2;
     //expect(value).toBe(0.3);           This won't work because of rounding error
     expect(value).toBeCloseTo(0.3); // This works.
@@ -70,11 +70,11 @@ test('adding floating point numbers', () => {
 
 // REFERENCES TO STRINGS
 
-test('there is no I in team', () => {
+test('Jest Example - there is no I in team', () => {
     expect('team').not.toMatch(/I/);
 });
 
-test('but there is a "stop" in Christoph', () => {
+test('Jest Example - but there is a "stop" in Christoph', () => {
     expect('Christoph').toMatch(/stop/);
 });
 
@@ -88,7 +88,7 @@ const shoppingList = [
     'beer',
 ];
 
-test('the shopping list has beer on it', () => {
+test('Jest Example - the shopping list has beer on it', () => {
     expect(shoppingList).toContain('beer');
     expect(new Set(shoppingList)).toContain('beer');
 });
@@ -99,7 +99,7 @@ function compileAndroidCode() {
     throw new Error('you are using the wrong JDK');
 }
 
-test('compiling android goes as expected', () => {
+test('Jest Example - compiling android goes as expected', () => {
     expect(compileAndroidCode).toThrow();
     expect(compileAndroidCode).toThrow(Error);
 
@@ -111,12 +111,12 @@ test('compiling android goes as expected', () => {
 // REFERENCES TO ASYNC/AWAYT
 // https://jestjs.io/docs/en/asynchronous
 
-// test('the data is peanut butter', async () => {
+// test('Jest Example - the data is peanut butter', async () => {
 //     const data = await fetchData();
 //     expect(data).toBe('peanut butter');
 // });
 
-// test('the fetch fails with an error', async () => {
+// test('Jest Example - the fetch fails with an error', async () => {
 //     expect.assertions(1);
 //     try {
 //         await fetchData();
@@ -127,10 +127,10 @@ test('compiling android goes as expected', () => {
 
 // You can combine async and await with .resolves or .rejects.
 
-// test('the data is peanut butter', async () => {
+// test('Jest Example - the data is peanut butter', async () => {
 //     await expect(fetchData()).resolves.toBe('peanut butter');
 // });
 
-// test('the fetch fails with an error', async () => {
+// test('Jest Example - the fetch fails with an error', async () => {
 //     await expect(fetchData()).rejects.toThrow('error');
 // });
