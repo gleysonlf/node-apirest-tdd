770fdc13aaaa0a75aca43202b856375657ced19a
Gleyson Lira <gleysonlf@live.com>
2020-01-24 20:50:13 -0300

    Deve inserir usuário com sucesso

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
 const app = require('express')();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.status(200).send())
 
@@ -7,6 +10,10 @@ app.get('/users', (req, res) => {
         { name: 'Gleyson Lira', email: 'gleysonlf@gmail.com' }
     ];
     res.status(200).json(users);
+})
+
+app.post('/users', (req, res) => {
+    res.status(201).json(req.body);
 })
 
 module.exports = app;
