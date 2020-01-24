6ddbc5fc8fb722008e1c0e7072d985e72aac9dd4
Gleyson Lira <gleysonlf@live.com>
2020-01-24 20:25:53 -0300

    Deve listar os usuários

--- src/app.js
+++ src/app.js
@@ -2,4 +2,11 @@ const app = require('express')();
 
 app.get('/', (req, res) => res.status(200).send())
 
+app.get('/users', (req, res) => {
+    const users = [
+        { name: 'Gleyson Lira', email: 'gleysonlf@gmail.com' }
+    ];
+    res.status(200).json(users);
+})
+
 module.exports = app;
